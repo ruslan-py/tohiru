56c296915b63be22b9b11b1211953435169c14e6
Rusik <ruslanlitvin.biz@gmail.com>
2021-05-09 18:54:27 +0300

    add words counter

--- tohiru/views.py
+++ tohiru/views.py
@@ -6,3 +6,9 @@ def about(request):
 
 def home(request):
     return render(request, 'index.html')
+
+def reversed(request):
+    ustext = request.GET['us_text']
+    len_words = len(ustext.split())
+    rev_text = ustext[::-1]
+    return render(request, 'reversed.html', {'us_text': rev_text, "lenwords": len_words})
